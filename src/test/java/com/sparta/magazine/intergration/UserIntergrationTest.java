8573ecac720f4eede89e67bb27072155bb58d229
이진서 <101075624+jinsDevelopment@users.noreply.github.com>
2022-05-05 10:53:52 +0900

    Update UserIntergrationTest.java

--- src/test/java/com/sparta/magazine/intergration/UserIntergrationTest.java
+++ src/test/java/com/sparta/magazine/intergration/UserIntergrationTest.java
@@ -26,88 +26,88 @@ import org.springframework.test.annotation.Rollback;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserIntergrationTest {
 
-	@Autowired
-	UserService userService;
+// 	@Autowired
+// 	UserService userService;
 
-	@Autowired
-	BCryptPasswordEncoder bCryptPasswordEncoder;
+// 	@Autowired
+// 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@Test
-	@Order(1)
-	@DisplayName("회원가입 성공")
-	@Transactional
-	void test1() {
-		// given
-		String username = "jinseoLee";
-		String password = "qwer1234";
-		String passwordCheck = "qwer1234";
-		String nickname = "jin";
+// 	@Test
+// 	@Order(1)
+// 	@DisplayName("회원가입 성공")
+// 	@Transactional
+// 	void test1() {
+// 		// given
+// 		String username = "jinseoLee";
+// 		String password = "qwer1234";
+// 		String passwordCheck = "qwer1234";
+// 		String nickname = "jin";
 
-		SignupRequestDto signupRequestDto = new SignupRequestDto(
-			username,
-			password,
-			passwordCheck,
-			nickname
-		);
+// 		SignupRequestDto signupRequestDto = new SignupRequestDto(
+// 			username,
+// 			password,
+// 			passwordCheck,
+// 			nickname
+// 		);
 
-		User user = userService.registerUser(signupRequestDto);
+// 		User user = userService.registerUser(signupRequestDto);
 
-		assertEquals(username,user.getUsername());
-		assertEquals(nickname,user.getNickname());
-		assertTrue(bCryptPasswordEncoder.matches(password,user.getPassword()));
-	}
+// 		assertEquals(username,user.getUsername());
+// 		assertEquals(nickname,user.getNickname());
+// 		assertTrue(bCryptPasswordEncoder.matches(password,user.getPassword()));
+// 	}
 
-	@Test
-	@Order(2)
-	@DisplayName("회원 가입 실패 - username 유효성 검사 실패1")
-	void test2() {
-		// given
-		String username = "null";
-		String password = "qwer1234";
-		String passwordCheck = "qwer1234";
-		String nickname = "jin";
+// 	@Test
+// 	@Order(2)
+// 	@DisplayName("회원 가입 실패 - username 유효성 검사 실패1")
+// 	void test2() {
+// 		// given
+// 		String username = "null";
+// 		String password = "qwer1234";
+// 		String passwordCheck = "qwer1234";
+// 		String nickname = "jin";
 
-		SignupRequestDto signupRequestDto = new SignupRequestDto(
-			username,
-			password,
-			passwordCheck,
-			nickname
-		);
+// 		SignupRequestDto signupRequestDto = new SignupRequestDto(
+// 			username,
+// 			password,
+// 			passwordCheck,
+// 			nickname
+// 		);
 
-		// when
-		Exception exception = assertThrows(RestException.class, () -> {
-			userService.registerUser(signupRequestDto);
-		});
+// 		// when
+// 		Exception exception = assertThrows(RestException.class, () -> {
+// 			userService.registerUser(signupRequestDto);
+// 		});
 
-		//then
-		assertEquals("사용할 수 없는 ID 입니다.",exception.getMessage());
-	}
+// 		//then
+// 		assertEquals("사용할 수 없는 ID 입니다.",exception.getMessage());
+// 	}
 
-	@Test
-	@Order(3)
-	@DisplayName("회원 가입 실패 - username 유효성 검사 실패2")
-	void test3() {
-		// given
-		String username = "jinseo";
-		String password = "qwer1234";
-		String passwordCheck = "qwer1234";
-		String nickname = "jin";
+// 	@Test
+// 	@Order(3)
+// 	@DisplayName("회원 가입 실패 - username 유효성 검사 실패2")
+// 	void test3() {
+// 		// given
+// 		String username = "jinseo";
+// 		String password = "qwer1234";
+// 		String passwordCheck = "qwer1234";
+// 		String nickname = "jin";
 
-		SignupRequestDto signupRequestDto = new SignupRequestDto(
-			username,
-			password,
-			passwordCheck,
-			nickname
-		);
+// 		SignupRequestDto signupRequestDto = new SignupRequestDto(
+// 			username,
+// 			password,
+// 			passwordCheck,
+// 			nickname
+// 		);
 
-		// when
-		Exception exception = assertThrows(RestException.class, () -> {
-			userService.registerUser(signupRequestDto);
-		});
+// 		// when
+// 		Exception exception = assertThrows(RestException.class, () -> {
+// 			userService.registerUser(signupRequestDto);
+// 		});
 
-		// then
-		assertEquals("중복된 사용자 ID 가 존재합니다.",exception.getMessage());
-	}
+// 		// then
+// 		assertEquals("중복된 사용자 ID 가 존재합니다.",exception.getMessage());
+// 	}
 
 }
 
